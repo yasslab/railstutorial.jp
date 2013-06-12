0d07a3406e5b9d46bda153d286fa87a5d71c5544
Yohei YASUKAWA <yasulab@gmail.com>
2013-06-12 15:16:34 +0900

    Redirect to railstutorial.jp if on Heroku

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'rubygems'
 require 'sinatra'
 
+site_url = ENV['SITE_URL'] || 'http://railstutorial.jp'
+
 get '/' do
+  redirect site_url, 301 if request.host =~ /\.herokuapp\.com$/
   erb :index
 end
 
 get '/index.html' do
-  redirect 'http://railstutorial.jp/'
+  redirect '/'
 end
 
