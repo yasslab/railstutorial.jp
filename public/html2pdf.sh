fecb8428c4f21768fb83c0f0f91cdd1929733884
Yohei YASUKAWA <yasulab@gmail.com>
2013-06-19 18:14:48 +0900

    Implement html2pdf script with PhantomJS

--- public/html2pdf.sh
+++ public/html2pdf.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# Collect HTML files in current directory, 
+# convert them into PDF with a script (rasterize.js) and PhantomJS.
+
 for file in `ls *.html`
 do
     echo "Creating $file.pdf ..."
-    phantomjs rasterize.js $file $file.pdf A4
+    phantomjs rasterize.js $file ~/Dropbox/Public/railstutorial.jp/$file.pdf A4
 done
