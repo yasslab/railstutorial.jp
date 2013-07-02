07e26243820037eb785f2f350bbd61f7b44ae7ce
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-02 15:19:10 +0900

    Remove needless sed in correct_htmls.sh

--- scripts/correct_htmls.sh
+++ scripts/correct_htmls.sh
@@ -8,13 +8,10 @@
 for file in `ls *.html`
 do
     cat $file | sed -e "s/http:\/\/railstutorial-ja.herokuapp.com/\/chapters/g" > $file.modified_1
-    cat $file.modified_1 | sed -e "s/,FITNESS/,<\/br>FITNESS/g" > $file.modified_2
-    cat $file.modified_2 | sed -e "s/WHETHER/<\/br>WHETHER/g" > $file.modified_3
-    cat $file.modified_3 | sed -e "s/IN CONNECTION WITH/<\/br>IN CONNECTION WITH/g" > $file.modified_4
-    cat $file.modified_4 | sed -e "s/フルサイズ/拡大/g" > $file.modified_5
-    cat $file.modified_5 | sed -e "s/テストドリブン/テスト駆動/g" > $file.modified_6
+    cat $file.modified_1 | sed -e "s/フルサイズ/拡大/g" > $file.modified_2
+    cat $file.modified_2 | sed -e "s/テストドリブン/テスト駆動/g" > $file.modified_3
     echo "corrected $file"
     if [ $? -eq 0 ]; then
-	mv $file.modified_6 $file
+	mv $file.modified_3 $file
     fi
 done
