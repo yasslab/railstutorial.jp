aae712a4407ff25e8c8db95d68d42b300337f3cc
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-07 14:26:03 +0900

    Add a script for correcting html tags for 3e

---
+++ public/books/4.0/correct_htmls.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Collect html files in the current directory,
+# then apply the following patches to each file:
+#   - Change absolute links into relative ones.
+#   - Align LICENSE by adding <br> tags.
+#   - Replace specific terminologies with appropriate Japanese ones.
+
+re_array=("s/railstutorial.org/railstutorial.jp/g" \
+          "s/,FITNESS/,<\/br>FITNESS/g" \
+	  "s/WHETHER/<\/br>WHETHER/g" \
+	  "s/IN CONNECTION WITH/<\/br>IN CONNECTION WITH/g" \
+	  "s/active_record_validations_callbacks/v3.2.13\/active_record_validations_callbacks/g" \
+)
+
+for file in `ls *.html`
+do
+    cp $file $file.modified_0
+    i=0
+    for re in "${re_array[@]}"
+    do
+	cat $file.modified_$i | sed -e "$re" > $file.modified_`expr $i + 1`
+	i=`expr $i + 1`
+    done
+    
+    if [ $? -eq 0 ]; then
+        mv $file.modified_$i $file
+    fi
+
+    echo "corrected $file"
+done
