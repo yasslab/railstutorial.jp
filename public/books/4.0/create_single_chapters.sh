89fa921588785206cb46477097068a8484b2e532
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-07 15:57:41 +0900

    Make each chapter single

--- public/books/4.0/create_single_chapters.sh
+++ public/books/4.0/create_single_chapters.sh
@@ -6,5 +6,6 @@ for chapter in `cat ../chapter_list.txt`
 do
     cat _head.html               >  "$chapter".html
     cat "$chapter"_fragment.html >> "$chapter".html
-    cat _foot.html               >  "$chapter".html
+    cat _foot.html               >> "$chapter".html
+    echo made $chapter.html a single chapter
 done
--- public/books/4.0/main.sh
+++ public/books/4.0/main.sh
@@ -3,4 +3,5 @@
 ./download_3e_htmls.sh
 ./correct_htmls.sh
 ./truncate_all.sh
+./create_single_chapters.sh
 
