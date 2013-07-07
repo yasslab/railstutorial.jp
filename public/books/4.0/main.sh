c6e038473b2667b02fdeb984c55617a73021cdba
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-07 16:02:20 +0900

    Add comments to main.sh for 3e

--- public/books/4.0/main.sh
+++ public/books/4.0/main.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Download HTML files from the original 3rd Edition,
+# then, correct and truncate them for Japanese Edition,
+# finally, make them a single chapter that can be imported from GTT.
+
 ./download_3e_htmls.sh
 ./correct_htmls.sh
 ./truncate_all.sh
