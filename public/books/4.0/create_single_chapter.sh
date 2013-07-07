a573f7887c0f57011eeff22c19df665bf98db7da
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-07 15:53:25 +0900

    Add a script and fragments to create single chapters

---
+++ public/books/4.0/create_single_chapter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Download and update HTML files from 'chapter_list.txt'.
+
+for chapter in `cat ../chapter_list.txt`
+do
+    cat _head.html               >  "$chapter".html
+    cat "$chapter"_fragment.html >> "$chapter".html
+    cat _foot.html               >  "$chapter".html
+done
