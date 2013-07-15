c622da10fde1bc60b2ccb9e3401cb9118d768104
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-15 15:47:55 +0900

    Replace Ruby RE with sed one to fix link bugs

--- public/books/4.0/create_book.sh
+++ public/books/4.0/create_book.sh
@@ -4,19 +4,32 @@
 # make anchor tags point to inside ones,
 # and create a book named 'book.html'.
 
+VERSION="?version=4.0"
+#VERSION=""
+
 echo "" >  book_fragment.html
 for chapter in `cat ../chapter_list.txt`
 do
-    cat "$chapter"_fragment.html >> book_fragment.html
+    #echo "s/$chapter$VERSION#top/$VERSION#cha-$chapter/g"
+    cat "$chapter"_fragment.html | \
+	sed -e "s/id=\"top\"/class=\"label\" id=\"cha-$chapter\"/g" \
+	>> book_fragment.html
 done
 echo "Created 'book_fragment.html'"
 
 # Make anchor tags point to the corresponding ones inside a book.
-cp  book_fragment.html book_fragment.modified.html
-cat book_fragment.modified.html | \
-    ruby -n -e 'puts $_.gsub(/(href=\")(?!http:\/\/)(.+?)([?|#])/, "\\1\\4")' \
-    > book_fragment.html
-rm  book_fragment.modified.html
+cp book_fragment.html book_fragment.modified_0.html
+i=0
+for chapter in `cat ../chapter_list.txt`
+do
+    cat book_fragment.modified_$i.html | \
+	sed -e "s/$chapter$VERSION#top/$VERSION#cha-$chapter/g" | \
+	sed -e "s/$chapter$VERSION/$VERSION/g" \
+	> book_fragment.modified_`expr $i + 1`.html
+    rm  book_fragment.modified_$i.html
+    i=`expr $i + 1`
+done
+mv book_fragment.modified_$i.html book_fragment.html
 echo "Corrected 'book_fragment.html'"
 
 # Create a 'book.html'
