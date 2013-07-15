a2aebb28a700033d030510454cb1351cfab80602
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-15 10:46:50 +0900

    Re-create a book from chapters

--- public/books/4.0/main.sh
+++ public/books/4.0/main.sh
@@ -4,9 +4,13 @@
 # then, correct and truncate them for Japanese Edition,
 # finally, make them a single chapter that can be imported from GTT.
 
+# For chapters
 ./download_3e_htmls.sh
 ./correct_htmls.sh
 ./truncate_all.sh
 ./create_single_chapters.sh
+
+# For book (generated from chapters)
 ./create_book.sh
+./correct_book.sh
 
