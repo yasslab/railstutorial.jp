768abe79596ba22a1a2837685c45fe8433e9d183
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-17 12:55:13 +0900

    Replace magic number w/ grep command

--- scripts/truncate.sh
+++ scripts/truncate.sh
@@ -4,8 +4,9 @@ if [ $# -eq 0 ]
 then
     echo "Usage: ./truncate.sh FILENAME"
 else
+    #Start=99
+    Start=`grep -n -w 'id="top"' $@ | cut -d ':' -f1`
     Line=`cat $@ | awk 'END {print NR }'`
-    Start=99
     End=`expr $Line - 3`
     cat $@ | head -n $End | tail -n +$Start
 fi
