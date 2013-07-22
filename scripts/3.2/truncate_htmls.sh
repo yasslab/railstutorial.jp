ac7c89780c03e68614c2bf2a3d1bc9d38491de97
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-22 18:18:01 +0900

    Align vertical line to make it readable

--- scripts/3.2/truncate_htmls.sh
+++ scripts/3.2/truncate_htmls.sh
@@ -6,7 +6,7 @@ for chapter in `cat chapter_list.txt`
 do
     Target="${chapter}_corrected.html"
     Start=`grep -n -w 'id="top"' $Target | cut -d ':' -f1`
-    End=`  grep -n -w '</body>' $Target | cut -d ':' -f1`
+    End=`  grep -n -w '</body>'  $Target | cut -d ':' -f1`
     End=`expr $End - 2`
     cat $Target | head -n $End | tail -n +$Start > ${chapter}_fragment.html
     echo "truncated: $Target \t -> ${chapter}_fragment.html"
