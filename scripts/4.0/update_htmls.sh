3c659459f1035aaebeda484ff7679bd859985df8
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-23 18:39:26 +0900

    Fix typo echo in update_htmls.sh

--- scripts/4.0/update_htmls.sh
+++ scripts/4.0/update_htmls.sh
@@ -4,11 +4,11 @@ for chapter in `cat chapter_list.txt`
 do
     mv $chapter.html ../../public/books/4.0/
     mv ${chapter}_fragment.html ../../public/books/4.0/
-    echo "move archive/ja/$chapter[_fragment].html \t -> ../../public/books/3.2/"
+    echo "move archive/ja/$chapter[_fragment].html \t -> ../../public/books/4.0"
 done
 
 for contents in `ls *contents*`
 do
     mv $contents ../../public/books/4.0/
-    echo "move archive/ja/$contents \t -> ../../public/books/3.2/"
+    echo "move archive/ja/$contents \t -> ../../public/books/4.0/"
 done
