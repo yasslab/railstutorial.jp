5bfa08ae73e2b97f42229f516a6fe37a6f3ab8c7
Yohei YASUKAWA <yasulab@gmail.com>
2013-07-27 04:03:35 +0900

    Align LICENSE for being readable from mobile for Rails 3.2

--- scripts/3.2/correct_htmls.sh
+++ scripts/3.2/correct_htmls.sh
@@ -10,9 +10,6 @@ re_array=("s/http:\/\/railstutorial-ja.herokuapp.com/\/chapters/g" \
           "s/chapters\/images/images/g" \	  
           "s/chapters\/pygments.css/stylesheets\/pygments.css/g" \
           "s/chapters\/polytexnic.css/stylesheets\/polytexnic.css/g" \
-          "s/,FITNESS/,<\/br>FITNESS/g" \
-	  "s/WHETHER/<\/br>WHETHER/g" \
-	  "s/IN CONNECTION WITH/<\/br>IN CONNECTION WITH/g" \
 	  "s/フルサイズ/拡大/g" \
 	  "s/テストドリブン/テスト駆動/g" \
 	  "s/active_record_validations_callbacks/v3.2.13\/active_record_validations_callbacks/g" \
